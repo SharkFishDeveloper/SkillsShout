17d19a374b5721487b45a88519570066eb116980
SharkFishDeveloper <116112291+SharkFishDeveloper@users.noreply.github.com>
2023-06-05 15:50:17 +0530

    created dashboard block

---
+++ reddit_june23/lib/features/dashboard/bloc/dashboard_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+part 'dashboard_event.dart';
+part 'dashboard_state.dart';
+
+class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
+  DashboardBloc() : super(DashboardInitial()) {
+    on<DashboardEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
